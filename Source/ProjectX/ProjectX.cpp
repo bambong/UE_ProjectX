879a66bd7eb6309587cea48201b6de1961722999
bambong <alsrbs2gh@gmail.com>
2024-03-12 23:59:11 +0900

    Init

---
+++ Source/ProjectX/ProjectX.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectX.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectX, "ProjectX" );
+ 
